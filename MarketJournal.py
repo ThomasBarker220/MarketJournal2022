0b56200ed82d65b4285bb20306c8f4ebdfddc6fe
ThomasBarker220 <james.barker@duke.edu>
2022-10-15 13:11:36 -0400

    Added comments for all code

--- MarketJournal.py
+++ MarketJournal.py
@@ -10,14 +10,31 @@ from datetime import date
 
 
 def stock_prices(data):
+    #Gets todays date
     today = date.today()
+
+    #Formats date
     day = today.strftime("%B %d, %Y")
+
+    #Initializes list with date as first entry
     daily_stocks = [day]
+
+    #Loops through every CNBC stock URL in the list data
     for link in data:
+
+        #Sends GET request to link
         r = requests.get(link)
+
+        #Preps HTML file
         soup = BeautifulSoup(r.content, 'html.parser')
+
+        #Finds span that contains price data
         price_tag = soup.find('span', class_="QuoteStrip-lastPrice")
+
+        #Gets text with price data
         price = price_tag.text
+
+        #If statement removes % sign from treasury yield data 
         if link == "https://www.cnbc.com/quotes/JP10Y?qsearchterm=Japan%2010" \
                    "%20year" or link == \
                 "https://www.cnbc.com/quotes/DE10Y?qsearchterm=bund%2010" or \
@@ -25,10 +42,13 @@ def stock_prices(data):
                 or link == "https://www.cnbc.com/quotes/US10Y?qsearchterm=US10":
             last = len(price)
             price = price[:last - 1]
+        #Appends price to stock list
         daily_stocks.append(price)
     print(daily_stocks)
+    #Returns stock list
     return daily_stocks
 
+#List of links with stocks
 data = ["https://www.cnbc.com/quotes/EUR=",
             "https://www.cnbc.com/quotes/GBP=",
             "https://www.cnbc.com/quotes/JPY=",
@@ -46,9 +66,15 @@ data = ["https://www.cnbc.com/quotes/EUR=",
             "https://www.cnbc.com/quotes/%40LCO.1?qsearchterm=brent",
             "https://www.cnbc.com/quotes/BTC.CB%3D?qsearchterm=bitcoin", ]
 
+#Reads CSV with stock data into a pandas data frame
 df = pd.read_csv(r'C:\Users\tbark\OneDrive - Duke University\BarkerAutomatedMarketJournal.csv')
+
+#Appends current day's stock list to data frame
 df.loc[len(df.index)] = stock_prices(data)
+
 print(df)
+
+#Writes updated pandas dataframe back into the CSV file
 df.to_csv(r'C:\Users\tbark\OneDrive - Duke University\BarkerAutomatedMarketJournal.csv', index=False)
 
 # if __name__ == '__main__':
